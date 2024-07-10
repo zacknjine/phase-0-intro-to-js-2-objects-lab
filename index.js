4eedc298fc2072c0c8fbabb5499b1f74f8196f47
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-11 01:09:41 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '123 Main St.'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+const newEmployee = deleteFromEmployeeByKey(employee, 'name');
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+
+
